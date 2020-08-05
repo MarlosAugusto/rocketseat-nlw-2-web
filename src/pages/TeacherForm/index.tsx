1246663e32c1382bfc3229a213c0b7dec564b5ea
MarlosAugusto <marlos.gomes@codengage.com>
2020-08-04 22:01:43 -0300

    feat: pages using the created components

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import PageHeader from '../../components/PageHeader'
 
-const TeacherForm = () => <h1>Teacher Form</h1>
+import './styles.css'
+
+const TeacherForm = () => (
+  <div id="page-teacher-header" className="container">
+    <PageHeader title="Que incrível que você quer das aulas." />
+  </div>
+)
 
 export default TeacherForm
