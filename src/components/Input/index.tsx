605dabbe014acc47c6abc2ae693da82f909ec8ff
MarlosAugusto <marlos.gomes@codengage.com>
2020-08-06 18:24:23 -0300

    feat: added Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import React, { InputHTMLAttributes } from 'react'
+
+import './styles.css'
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+const Input: React.FC<IInputProps> = ({ name,label, ...rest }) => (
+  <div className="input-block">
+    <label htmlFor={name}>{label}</label>
+    <input id={name} {...rest} />
+  </div>
+)
+
+export default Input
