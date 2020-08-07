f35b24b3b6e51c694e2b08c12a87bc044649ecec
MarlosAugusto <marlos.gomes@codengage.com>
2020-08-07 00:24:55 -0300

    refactor: component TeacherItem

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,47 +3,54 @@ import React from 'react'
 import './styles.css'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api'
 
 export interface IProffyProps {
   avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  user_id: number;  
   name: string;
-  discipline: string;
-  description: string;
-  extraDescription?: string;
-  priceHour: number;
+  subject: string;
+  whatsapp: string;
 }
 
-const TeacherItem: React.FC<IProffyProps> = (props) => (  
-  <article className="teacher-item">
-  <header>
-    <img src={props.avatar} alt={props.name}/>
-    <div>
-      <strong>{props.name}</strong>
-      <span>NodeJS</span>
-    </div>
-  </header>
+const TeacherItem: React.FC<IProffyProps> = (props) => {
+  const createNewConnection = () => {
+    api.post('connections', {
+      user_id: props.user_id
+    })
+  }
+  return (  
+    <article className="teacher-item">
+      <header>
+        <img src={props.avatar} alt={props.name}/>
+        <div>
+          <strong>{props.name}</strong>
+          <span>{props.subject}</span>
+        </div>
+      </header>
 
-  <p>
-    {props.description}
-    {props.extraDescription && (
-      <>
-        <br /><br />
-        {props.extraDescription}
-      </>
-    )}
-  </p>
+      <p>{props.bio}</p>
 
-  <footer>
-    <p>
-      Preço/hora
-      <strong>R$ {(props.priceHour.toFixed(2).replace('.',','))}</strong>
-    </p>
-    <button type="button">
-      <img src={whatsappIcon} alt="whatsapp icon"/>
-      Entrar em contato
-    </button>
-  </footer>
-</article>
-)
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {(props.cost.toFixed(2).replace('.',','))}</strong>
+        </p>
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/${props.whatsapp}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={whatsappIcon} alt="whatsapp icon"/>
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  )
+}
 
 export default TeacherItem
